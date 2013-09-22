abc143a4b61280d9595e723734b74364f00852dd
Pali Rohár <pali.rohar@gmail.com>
2013-09-22 12:43:12 +0200

    Include jonwil changes

--- include/systemui/modechange-dbus-names.h
+++ include/systemui/modechange-dbus-names.h
@@ -20,31 +20,13 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA
  */
-#if 0
-typedef enum {
-  DEVMODE_NORMAL = 0, /* Normal mode */
-  DEVMODE_FLIGHT,     /* Flight mode */
-  DEVMODE_OFFLINE,    /* Offline mode; unsupported! */
-  DEVMODE_INVALID     /* Should *never* occur! */
-} devmode_t;
-#endif
-
-#if 0
-typedef enum {
-  MODE_NORMAL = 0, /* Normal mode */
-  MODE_FLIGHT,     /* Flight mode */
-  MODE_OFFLINE,    /* Offline mode; unsupported! */
-  MODE_INVALID     /* Should *never* occur! */
-} mode_t;
-#endif
-
 #define SYSTEMUI_MODECHANGE_OPEN_REQ "modechange_open"
 
 #define SYSTEMUI_MODECHANGE_CLOSE_REQ "modechange_close"
 
 typedef enum {
-	MODECHANGE_TO_NORMALMODE = 0,
-	MODECHANGE_TO_FLIGHTMODE = 1
+	MODECHANGE_TO_FLIGHTMODE = 0,
+	MODECHANGE_TO_NORMALMODE = 1
 } modechange_t;
 
 typedef enum {
